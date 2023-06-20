637b99ffea385acbf67725b60431c68ce29902a9
elias rizik <eliasrizik@eliass-MacBook-Air.local>
2023-06-20 21:10:31 +0300

    add search input

--- client/src/components/Messages/SideBarMessages/SideBarMessages.tsx
+++ client/src/components/Messages/SideBarMessages/SideBarMessages.tsx
@@ -1,9 +1,28 @@
+import React from 'react';
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    width: '100%',
+  },
+}));
+
 const SideBarMessages = () => {
+  const classes = useStyles();
+
   return (
-    <div className="sidebar__messages w-1/5">
-      <div className="">search</div>
+    <div className="sidebar__messages w-1/5 mr-2 ml-5">
+      <TextField
+        id="standard-basic"
+        label="Standard"
+        variant="standard"
+        className={classes.input}
+      />
+
       <div className="h-full">Messages</div>
     </div>
   );
 };
+
 export default SideBarMessages;
